c1714293abfeb26fd322a6fb3f3d07de86991dff
Josh Ziegler <josh.s.ziegler@gmail.com>
2011-02-24 19:14:19 -0500

    Removed Header.

--- zig/search/search.py
+++ zig/search/search.py
@@ -1,16 +1,3 @@
-# search.py
-# ---------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
-
-"""
-In search.py, you will implement generic search algorithms which are called 
-by Pacman agents (in searchAgents.py).
-"""
-
 import util
 
 class SearchProblem:
