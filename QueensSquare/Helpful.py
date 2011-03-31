7b474ceb9b0571cbc8a3f68c4ec81f488ab88aa6
Josh Ziegler <josh.s.ziegler@gmail.com>
2011-03-31 09:58:43 -0400

    Added the start of the HillClimber agent to QueensSquare.

--- QueensSquare/Helpful.py
+++ QueensSquare/Helpful.py
@@ -1,4 +1,7 @@
-#Contains useful functions
+"""Contains useful functions
+   """
+import random
+
 def Slope(PosA, PosB):
     """Calculates the slope between two points"""
     if PosA[0] == PosB[0]:
@@ -9,3 +12,33 @@ def Slope(PosA, PosB):
                 float(PosB[1])) /\
                (float(PosA[0]) -\
                 float(PosB[0]))
+
+def get_board_model(size):
+    """Creates a two-dimensional dictionary of True/False values which represents the game board.
+
+    False means that there is no queen there, and True means that there is.
+       """
+    model = {}
+    for row in xrange(size):
+        model[row] = {}
+        for col in xrange(size):
+            model[row][col] = False 
+    return model
+
+def place_pieces_randomly(Board):
+    size = Board.GetSize() 
+    size_list = range(size)
+    model = get_board_model(size)
+
+    for queen in Board.Queens:
+        while True:
+            row = random.choice(size_list)
+            col = random.choice(size_list)
+            if not model[row][col]: # if no queen exists here yet
+                model[row][col] = True 
+                queen.Position = (row, col)
+                break # place the next queen
+
+
+
+
