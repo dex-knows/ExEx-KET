f33920e94d53c934f5cc5026e65ca553b1ac3be2
Dex <dex.knows10@gmail.com>
2011-04-04 18:03:46 -0400

    It kicks ass.

--- QueensSquare/Agent.py
+++ QueensSquare/Agent.py
@@ -3,32 +3,114 @@
 import random
 
 from GameState import Game
+from GameState import Queen
 from Helpful import * 
 
 def RandomAgent(Board):
     """An agent that will try to randomly place the queens to win."""
+    Rows = range(Board.GetSize())
     a = 0
     for ThisQueen in Board.Queens:
-        ThisQueen.Position = (a, a)
+        ThisQueen.Position = (a, assign_random(Rows))
         a = a + 1
-    RowGen = random.random
     while (Board.IsEndState() == False):
         Rows = range(Board.GetSize())
-        for ThisQueen in Board.Queens:
-            ThisRow = int(RowGen() * len(Rows))
-            ThisQueen.Position = (ThisQueen.Position[0], Rows[ThisRow])
-            del Rows[ThisRow]
+        assign_positions(Board.Queens, Rows)
 
-def GetFitness(Board):
+def assign_positions(Queens, Rows):
+    """Randomly assigns positions to Queens"""
+    for ThisQueen in Queens:
+        ThisQueen.Position = (ThisQueen.Position[0], assign_random(Rows))
+
+def assign_random(Items):
+    """Randomly returns an item from the list of items and removes it from the lise"""
+    ItemIndex = int(random.random() * len(Items))
+    Item = Items[ItemIndex]
+    del Items[ItemIndex]
+    return Item    
+
+def GetFitness(Queens):
     """Calculates the fitness of the current layout"""
-    
-    pass
+    Fitness = 0
+    for QueenA in xrange(len(Queens)):
+        for QueenB in xrange(QueenA + 1, len(Queens)):
+            slope = Slope(Queens[QueenA].Position,\
+                          Queens[QueenB].Position)
+            #Checks to see if Queens are on diagonally, horizontally
+            # or vertically aligned.
+            if (slope != -1) and (slope != 1) and (slope != 0):
+                Fitness = Fitness + 1
+    return Fitness
 
 def FitnessGoal(Size):
     """Returns the goal fitness based on the size of the game"""
-    #formulate based on combination Size choose 2.
+    #formula based on combination Size choose 2.
     #perfect fitness is one in which all pairs of queens don't attack
     return (Size * (Size -1))/2  
+
+def SingleGenetic(Board, PopSize):
+    """Genetic algorithm with an 'asexual' approach using the specified population size"""
+    Populations = []
+    #Controls how big of a population there should be.
+    for ThisPop in xrange(PopSize):
+        Queens = []
+        Rows = range(Board.GetSize())
+        #Creates queens for each population.
+        for a in xrange(Board.GetSize()):
+            Queens[a:] = [Queen((a, assign_random(Rows)))]
+        Populations[ThisPop:] = [Queens]
+    
+    GoalFit = FitnessGoal(Board.GetSize())
+    BestFit = 0
+
+    while (BestFit != GoalFit):
+        BestFit = 0
+        ThisFit = 0
+        NextGen = []
+        for ThisPop in xrange(len(Populations)):
+            #Collect fitness info on the global populations.
+            ThisFit = GetFitness(Populations[ThisPop])
+            #If the fitness matches the goal fitness check to see if it is the end state.
+            if ThisFit == GoalFit:
+               for ThisQueen in xrange(len(Board.Queens)):
+                   Board.Queens[ThisQueen].Position = \
+                                   Populations[ThisPop][ThisQueen].Position
+               if Board.IsEndState():
+                  BestFit = GoalFit
+            #Check to see if there is a new best population and reset the next generation.
+            if ThisFit > BestFit:
+                BestFit = ThisFit
+                NextGen = []
+            #Adds any best populations to the potential next generation.
+            if ThisFit == BestFit:
+               NextGen.append(Populations[ThisPop])
+    
+        #Randomly select one of the best Populations to create the next generation with.
+        Seed = assign_random(NextGen)
+
+        Populations = []
+        NewPop = []
+        
+        #Create as many new populations as there were previously.
+        for ThisPop in xrange(PopSize):
+            #The new populations will be a mutation/premutation of the seed, such that
+            #two of the columns row positions will be flip flopped.
+            #this will preserve the one row, one column, one queen setup while allowing
+            #for 'random' mutation.
+            Cols = range(Board.GetSize())
+            MutColA = assign_random(Cols) 
+            MutColB = assign_random(Cols)    
+            #Copy the seed into a new population.        
+            for a in xrange(len(Seed)):
+                NewPop[len(NewPop):] = [Queen(Seed[a].Position)]
+            #preform the mutation.
+            Temp = NewPop[MutColA].Position[1]
+            NewPop[MutColA].Position = (MutColA, NewPop[MutColB].Position[1])
+            NewPop[MutColB].Position = (MutColB, Temp)
+            #add the new population to the global population.
+            Populations[len(Populations):] = [NewPop]
+            NewPop = []
+        Board.IsEndState()
 
 def HillClimber(Board):
     """An agent that tries moving one piece at a time to win.  It will start over if
@@ -138,5 +220,3 @@ def HillClimber(Board):
                 queen_model = place_pieces_randomly(Board) # moves the queens on Board
                 reset_strikes(strikes, queen_model)
                 old_score = None
-            
-    
