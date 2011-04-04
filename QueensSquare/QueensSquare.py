18d855397451c4ff5fbf941a69ea3f9855f36714
Dex <dex.knows10@gmail.com>
2011-04-04 18:03:46 -0400

    It kicks ass.

--- QueensSquare/QueensSquare.py
+++ QueensSquare/QueensSquare.py
@@ -8,35 +8,16 @@ from Agent import *
 if __name__ == "__main__":
     locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
-#    a = Game()
-#    a.NewGame(4)
-#    a.MoveQueen(0, (0, 2))
-#    a.MoveQueen(1, (1, 0))
-#    a.MoveQueen(2, (2, 3))
-#    a.MoveQueen(3, (3, 1))
-#    print a.IsEndState()
-#    a.MoveQueen(0, (2, 1))
-#    print a.IsEndState()
-#    a.NewGame(8)
-#    a.MoveQueen(0, (0, 1))
-#    a.MoveQueen(1, (1, 3))
-#    a.MoveQueen(2, (2, 5))
-#    a.MoveQueen(3, (3, 7))
-#    a.MoveQueen(4, (4, 2))
-#    a.MoveQueen(5, (5, 0))
-#    a.MoveQueen(6, (6, 6))
-#    a.MoveQueen(7, (7, 4))
-#    print a.IsEndState()
-
-    for game_size in range(4,14):
+    for game_size in range(20,25):
         
         Test = Game()
         Test.NewGame(game_size)
-        NumRuns = 100
+        NumRuns = 10
         TotalAttempts = 0
         for x in xrange(NumRuns): 
             #RandomAgent(Test)
-            HillClimber(Test)
+            #HillClimber(Test)
+            SingleGenetic(Test, x + 2)
             TotalAttempts +=  Test.GetAttempts()
 
         print "Game Size:", game_size
