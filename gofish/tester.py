364dfe9cfad2f7387805cbec23211219a7976d36
Josh Ziegler <josh.s.ziegler@gmail.com>
2011-04-07 18:06:49 -0400

    Non-working commit which tries to dynamically load an agent.

--- gofish/tester.py
+++ gofish/tester.py
@@ -1,9 +1,12 @@
 from state import State
 
-from agents.RandomAgent import RandomAgent
+from agents import *
 
-def run_game():
-    josh = RandomAgent("josh")
+VERSION = ".10"
+
+def run_game(agent):
+    #josh = getattr(agents, agent)("agent")
+    josh = RandomAgent("AdverseAgent")
     ryan = RandomAgent("ryan")
     mitch = RandomAgent("mitch")
     helen = RandomAgent("helen")
@@ -12,13 +15,12 @@ def run_game():
     player_loss_margin = s.start_new_game(False, False, False, False, [josh,ryan,mitch,helen])
     return player_loss_margin
 
-
-if __name__ == "__main__":
+def run_test(agent, iterations):
     number_of_games = 0
     total_loss_margins = {} 
 
-    for c in xrange(1000):
-        results = run_game()
+    for c in xrange(iterations):
+        results = run_game(agent)
         number_of_games += 1
 
         for player_name, loss_margin in results.iteritems():
@@ -30,4 +32,26 @@ if __name__ == "__main__":
 
     for player_name, loss_margin in total_loss_margins.iteritems():
         print player_name, "lost by", float(loss_margin)/float(number_of_games), "on average"
-    
+ 
+
+if __name__ == "__main__":
+    import os
+    from optparse import OptionParser
+    usage = "usage: %prog [options] agent \n"
+    usage += '\n'
+    usage += "available agents: "
+    for method in os.listdir("agents"):
+        if 'Agent' in method:
+            usage += '\n    ' + method.replace('.py','')
+
+    parser = OptionParser(usage, version="%prog "+VERSION)
+    parser.add_option("-i", "--iterations", dest="iterations", help="how many games to run for the test", default=1000)
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
+    else:
+        agent = args[0]
+        run_test(agent, options.iterations)
+
+
